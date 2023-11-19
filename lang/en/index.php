09943ddba97e228ab57ca295d27ad5d2a60d6d02
RuslanButenko <foxhand@proton.me>
2023-11-19 19:35:25 +0200

    ✨➕ Додати мовні рядки для індексу
    Додано файл з мовними рядками для індексу з наступними значеннями:
    - 'home' => 'Про мене'
    - 'name' => 'Foxhand'
    
    Signed-off-by: RuslanButenko <foxhand@proton.me>

---
+++ lang/en/index.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Index Language Lines
+        |--------------------------------------------------------------------------
+        |
+        | The following language lines are used by the index library to build
+        | the simple index links. You are free to change them to anything
+        | you want to customize your views to better match your application.
+        |
+        */
+
+        'home' => 'About me',
+        'name' => 'Foxhand',
+
+];
