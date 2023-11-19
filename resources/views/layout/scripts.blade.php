771931253eb35fa07be87ec79f6c5fe9e029ba20
RuslanButenko <foxhand@proton.me>
2023-11-19 19:32:38 +0200

    ✨ Додав файл scripts.blade.php у папку resources/views/layout ✨
    Signed-off-by: RuslanButenko <foxhand@proton.me>

---
+++ resources/views/layout/scripts.blade.php
@@ -0,0 +1,22 @@
+<!-- Vendor -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery.appear/jquery.appear.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery.cookie/jquery.cookie.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery.validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery.gmap/jquery.gmap.min.js') }}"></script>
+<script src="{{ asset('vendor/lazysizes/lazysizes.min.js') }}"></script>
+<script src="{{ asset('vendor/isotope/jquery.isotope.min.js') }}"></script>
+<script src="{{ asset('vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('vendor/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('vendor/vide/jquery.vide.min.js') }}"></script>
+<script src="{{ asset('vendor/vivus/vivus.min.js') }}"></script>
+<!-- Theme Base, Components and Settings -->
+<script src="{{ asset('js/theme.js') }}"></script>
+
+<!-- Theme Custom -->
+<script src="{{ asset('js/custom.js') }}"></script>
+<!-- Theme Initialization Files -->
+<script src="{{ asset('js/theme.init.js') }}"></script>
