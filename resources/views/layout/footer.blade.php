c3e9da600248e248475621ca9fcf80547ae7e8b9
RuslanButenko <foxhand@proton.me>
2023-11-19 19:33:43 +0200

    📝 Доданий новий файл footer.blade.php з вмістом футера для веб-сайту. Футер містить авторські права та поточний рік.
    Signed-off-by: RuslanButenko <foxhand@proton.me>

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="footer-copyright footer-copyright-style-2 pt-4 pb-5">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="mb-0">{{ trans('index.name') }} © {{ date('Y') }}. All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</div>
