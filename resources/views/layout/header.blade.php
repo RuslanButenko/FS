05e4090f6017e9103c87950175ec19dcdc498601
RuslanButenko <foxhand@proton.me>
2023-11-19 19:32:56 +0200

    🌱 Додано файл розмітки шапки (header.blade.php) до папки resources/views/layout.
    Signed-off-by: RuslanButenko <foxhand@proton.me>

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,34 @@
+<div class="header-body border-top-0 box-shadow-none">
+    <div class="header-container header-container-md container">
+        <div class="header-row">
+            <div class="header-column">
+                <div class="header-row">
+                    <div class="header-logo">
+                        <a href="/"><img alt="Foxhand" width="100" height="48" data-sticky-width="82" data-sticky-height="40" data-sticky-top="0" src="{{ asset('img/logo-default-slim.png') }}"></a>
+                    </div>
+                </div>
+            </div>
+{{--            <div class="header-column justify-content-end">--}}
+{{--                <div class="header-row">--}}
+{{--                    <div class="header-nav header-nav-line header-nav-bottom-line header-nav-bottom-line-no-transform header-nav-bottom-line-active-text-dark header-nav-bottom-line-effect-1 order-2 order-lg-1">--}}
+{{--                        <div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">--}}
+{{--                            <nav class="collapse">--}}
+{{--                                <ul class="nav nav-pills" id="mainNav">--}}
+{{--                                    <li class="dropdown">--}}
+{{--                                        <a class="dropdown-item dropdown-toggle" href="/">--}}
+{{--                                            Home--}}
+{{--                                        </a>--}}
+{{--                                    </li>--}}
+
+{{--                                </ul>--}}
+{{--                            </nav>--}}
+{{--                        </div>--}}
+{{--                        <button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">--}}
+{{--                            <i class="fas fa-bars"></i>--}}
+{{--                        </button>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+        </div>
+    </div>
+</div>
